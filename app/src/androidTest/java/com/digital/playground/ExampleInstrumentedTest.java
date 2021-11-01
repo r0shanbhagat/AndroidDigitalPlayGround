ba334e505808b272a2dc4d132b455e8fd565bcd9
Roshan Bhagat <softengg.roshan@gmail.com>
2021-11-01 14:44:53 +0530

    applicationId Updated

--- app/src/androidTest/java/com/digital/playground/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/digital/playground/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.demo.assignment", appContext.getPackageName());
+        assertEquals("com.digital.playground", appContext.getPackageName());
     }
 }
