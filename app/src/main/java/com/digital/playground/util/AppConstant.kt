b9a6aa942734ee33fc80a69b26695dcacde10555
Roshan Bhagat <softengg.roshan@gmail.com>
2021-11-10 19:26:58 +0530

    Added ViewModelUnit Test cases

--- app/src/main/java/com/digital/playground/util/AppConstant.kt
+++ app/src/main/java/com/digital/playground/util/AppConstant.kt
@@ -1,7 +1,5 @@
 package com.digital.playground.util
 
-interface AppConstant {
-    companion object {
-        const val ARGS_JOKES = "args_jokes"
-    }
+object AppConstant {
+    const val ARGS_JOKES = "args_jokes"
 }
